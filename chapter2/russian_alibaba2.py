d02b8970b9bdd3eb536098143c936c5b9e3ef008
Jamshid Masharipov <masharipovjorabek0@gmail.com>
2024-04-20 20:43:43 +0500

    adding yaml  writer every Category names with sub,parent,childe  names
    adding yaml  writer every Category names with sub,parent,childe  names

--- chapter2/russian_alibaba2.py
+++ chapter2/russian_alibaba2.py
@@ -1,6 +1,7 @@
 import requests
+import os
 from bs4 import BeautifulSoup
-
+import yaml
 def parse_russian_alibaba(url):
     response = requests.get(url)
     response.encoding = 'utf-8'
@@ -17,16 +18,25 @@ def parse_russian_alibaba(url):
             child_categories = child_category_container.find_all('li')
             for child_category in child_categories:
                 child_category_name = child_category.find('a').get_text(strip=True)
-                all_categories.append(f"{parent_category_name}\\{sub_category_name}\\{child_category_name}")
+                all_categories.append({"parent": parent_category_name,
+                                       "sub": sub_category_name,
+                                       "child": child_category_name})
 
-    # print(all_categories)
-    return  all_categories
+    return all_categories
 
-categories=parse_russian_alibaba(url='https://russian.alibaba.com/products')
-
+categories = parse_russian_alibaba(url='https://russian.alibaba.com/products')
 #  Reason bunaqa format kegusida file paths sifatida foydalanishimiz uchun shu formatda yigildi yani mhtml yoki html larni saveing location sifatida
-for category in categories:
-    print(category)
 
+# you can see every Category names
 
+# for category in categories:
+#     print(category)
 
+# Write categories to a YAML file
+path='categories.yaml'
+if os.path.exists(path):
+    print(f'The file:{path} already exists')
+else:
+    print(f'The file:{path} does not exist')
+    with open('categories.yaml', 'w') as file:
+        yaml.dump(categories, file)
