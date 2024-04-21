73415d1a3b16f392bf864da00921ca72b949ff3a
Jamshid Masharipov <masharipovjorabek0@gmail.com>
2024-04-22 02:19:50 +0500

    #Step2  Fixed problem Categorycreator Rest API

--- chapter2/Category creator with API.py
+++ chapter2/Category creator with API.py
@@ -1,44 +1,85 @@
-import time
+import requests
 import os
-import yaml
+from bs4 import BeautifulSoup
 from woocommerce import API
+import yaml
+#
+# # WC API ni yaratish
+# wcapi = API(
+#     url="https://globmarketplace.com/ru/",
+#     consumer_key="ck_3a6bd6bf3484322bc28eca2787af2332df2e29ec",
+#     consumer_secret="cs_3e7f36edc1c1012786049cc48647d55b62358d8c"
+# )
+def parse_russian_alibaba(url):
+    response = requests.get(url)
+    response.encoding = 'utf-8'
+    soup = BeautifulSoup(response.text, 'html.parser')
 
+    categories_container = soup.find_all('div', class_='item util-clearfix')
+    all_categories = {}
+    for category_container in categories_container:
+        parent_category_name = category_container.find('h3').text.strip()
+        sub_categories_container = category_container.find_all('div', class_='sub-item')
+        all_categories[parent_category_name] = {}
+        for sub_category_container in sub_categories_container:
+            sub_category_name = sub_category_container.find('h4', class_='sub-title').find('a').get_text(strip=True)
+            child_category_container = sub_category_container.find('ul', class_='sub-item-cont util-clearfix')
+            all_categories[parent_category_name][sub_category_name] = []
+            child_categories = child_category_container.find_all('li')
+            for child_category in child_categories:
+                child_category_name = child_category.find('a').get_text(strip=True)
+                all_categories[parent_category_name][sub_category_name].append(child_category_name)
+
+    return all_categories
+#
+
+# WC API ni yaratish
 wcapi = API(
     url="https://globmarketplace.com/ru/",
-    consumer_key="ck_3a6bd6bf3484322bc28eca2787af2332df2e29ec",
-    consumer_secret="cs_3e7f36edc1c1012786049cc48647d55b62358d8c"
-)
+    consumer_key="ck_019eeb344f39579df759e04f6f96c522fe3b896b",
+    consumer_secret="cs_5127afd22c791c8ce0d56a596b8f7d9d959d0c96"
+    )
+def create_categories(categories, parent=0):
+    # Agar categories ro'yxati ro'yxat bo'lsa
+    if isinstance(categories, list):
+        for sub_categories in categories:
+            create_categories(sub_categories, parent=parent)
+    # Agar categories ro'yxati obyekt bo'lsa
+    elif isinstance(categories, dict):
+        for category_name, sub_categories in categories.items():
+            # Kategoriyani yaratish
+            data = {
+                "name": category_name,
+                "parent": parent
+            }
+            response = wcapi.post("products/categories", data).json()
+            category_id = response.get("id")
 
-def create_category(category_data, parent_category_id=0):
-    # Check if category exists
-    existing_categories = wcapi.get("products/categories").json()
-    category_name = category_data["name"]
-    for category in existing_categories:
-        if category["name"] == category_name:
-            print(f"Category '{category_name}' already exists.")
-            return category["id"]
+            print(f"Category '{category_name}' created with ID {category_id}")
 
-    # Create new category
-    image_url = category_data.get("image", "").get("src", "")
-    data = {
-        "name": category_name,
-        "parent": parent_category_id,
-        "image": {"src": image_url}
-    }
-    try:
-        response = wcapi.post("products/categories", data).json()
-        category_id = response.get("id")
-        print(f"Category '{category_name}' created successfully with ID: {category_id}")
-        return category_id
-    except Exception as e:
-        print(f"Error creating category '{category_name}': {e}")
-        return None
+            # Agar ichki kategoriyalar mavjud bo'lsa, ularga qo'shish
+            if sub_categories:
+                create_categories(sub_categories, parent=category_id)
+    else:
+        print("Invalid categories format!")
 
-with open("categories.yaml", "r") as file:
-    categories = yaml.safe_load(file)
+def main():
+    # Kategoriyalarni olish
+    categories = parse_russian_alibaba(url='https://russian.alibaba.com/products')
 
-for category_data in categories:
-    create_category(category_data)
-    # print(category_data)
+    # WC API ni yaratish
+    wcapi = API(
+        url="https://globmarketplace.com/ru/",
+        consumer_key="ck_3a6bd6bf3484322bc28eca2787af2332df2e29ec",
+        consumer_secret="cs_3e7f36edc1c1012786049cc48647d55b62358d8c"
+    )
+
+    # Bosh kategoriyalarni yaratish
+    create_categories(categories)
+
+if __name__ == "__main__":
+    main()
+
+
 
 
